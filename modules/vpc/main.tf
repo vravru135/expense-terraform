c9ded1f9f06fad784f5ef807c73b71794a3d45f2
vravru135 <vravru135@gmail.com>
2024-03-08 11:59:47 +0530

    Adding shell Topics

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,67 +1,63 @@
 resource "aws_vpc" "main" {
- cidr_block = var.vpc_cidr_block
- tags       = merge(var.tags, { Name = var.env})
+  cidr_block = var.vpc_cidr_block
+  tags = merge(var.tags, { Name = var.env })
 }
 
-## These subnet blocks needs improvement in terms of code dry.
 resource "aws_subnet" "public" {
-  count      = length(var.public_subnets)
+  count = length(var.public_subnets)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets[count.index]
-  tags       = merge(var.tags, { Name = "public-subnet" })
+  tags = merge(var.tags, { Name = "public_subnet" })
   availability_zone = var.azs[count.index]
 }
 
 resource "aws_subnet" "web" {
-  count      = length(var.web_subnets)
+  count = length(var.web_subnets)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.web_subnets[count.index]
-  tags       = merge(var.tags, { Name = "web-subnet" })
+  tags = merge(var.tags, { Name = "web_subnet" })
   availability_zone = var.azs[count.index]
 }
 
 resource "aws_subnet" "app" {
-  count      = length(var.app_subnets)
+  count = length(var.app_subnets)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.app_subnets[count.index]
-  tags       = merge(var.tags, { Name = "app-subnet" })
+  tags = merge(var.tags, { Name = "app_subnet" })
   availability_zone = var.azs[count.index]
 }
 
 resource "aws_subnet" "db" {
-  count      = length(var.db_subnets)
+  count = length(var.db_subnets)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.db_subnets[count.index]
-  tags       = merge(var.tags, { Name = "db-subnet" })
+  tags = merge(var.tags, { Name = "db_subnet" })
   availability_zone = var.azs[count.index]
 }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, { Name = "public" })
-}
-
-  route {
-    cidr_block = var.default_vpc_cidr
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
 }
 
 resource "aws_route_table" "web" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, { Name = "web" })
-}
 
- route {
-    cidr_block = "0.0.0.0/0"
+  route {
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
-}
 
   route {
     cidr_block = var.default_vpc_cidr
@@ -72,13 +68,11 @@ resource "aws_route_table" "web" {
 resource "aws_route_table" "app" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, { Name = "app" })
-}
 
- route {
-    cidr_block = "0.0.0.0/0"
+  route {
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
-}
 
   route {
     cidr_block = var.default_vpc_cidr
@@ -89,13 +83,11 @@ resource "aws_route_table" "app" {
 resource "aws_route_table" "db" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, { Name = "db" })
-}
 
- route {
-    cidr_block = "0.0.0.0/0"
+  route {
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
-}
 
   route {
     cidr_block = var.default_vpc_cidr
@@ -104,38 +96,37 @@ resource "aws_route_table" "db" {
 }
 
 resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
+  count = length(aws_subnet.public)
   subnet_id      = aws_subnet.public.*.id[count.index]
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "web" {
-  count          = length(aws_subnet.web)
+  count = length(aws_subnet.web)
   subnet_id      = aws_subnet.web.*.id[count.index]
   route_table_id = aws_route_table.web.id
 }
 
 resource "aws_route_table_association" "app" {
-  count          = length(aws_subnet.app)
+  count = length(aws_subnet.app)
   subnet_id      = aws_subnet.app.*.id[count.index]
   route_table_id = aws_route_table.app.id
 }
 
 resource "aws_route_table_association" "db" {
-  count          = length(aws_subnet.db)
+  count = length(aws_subnet.db)
   subnet_id      = aws_subnet.db.*.id[count.index]
   route_table_id = aws_route_table.db.id
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags   = merge(var.tags, { Name = "igw" })
+  tags = merge(var.tags, { Name = "igw" })
 }
 
 resource "aws_eip" "ngw" {
   domain = "vpc"
 }
-
 resource "aws_nat_gateway" "ngw" {
   allocation_id = aws_eip.ngw.id
   subnet_id     = aws_subnet.public.*.id[0]
@@ -145,7 +136,7 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = var.account_id
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
-  auto_accept   = true
+  auto_accept = true
   tags          = merge(var.tags, { Name = "peer-for-${var.env}-vpc-to-default-vpc" })
 }
 
@@ -154,14 +145,3 @@ resource "aws_route" "default-vpc-peer-route" {
   destination_cidr_block    = var.vpc_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
-
-# Testing
-
-#resource "aws_instance" "test" {
-#   ami                    = "ami-0f3c7d07486cad139"
-#   instance_type          = "t3.micro"
- #  subnet_id = aws_subnet.app.*.id[0]
-  # tags = {
-   #  Name = "test"
-   #}
-#}
