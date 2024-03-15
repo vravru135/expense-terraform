6a96bd99b880b07153851abbcb5d1f66e9a9fa1d
vravru135 <vravru135@gmail.com>
2024-03-15 20:31:41 +0530

    Adding shell Topics

--- modules/app/data.tf
+++ modules/app/data.tf
@@ -1,5 +1,12 @@
+/*data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}*/
+
+
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["290654222953"]
+  name_regex       = "golden-ami"
+  owners           = ["469562195624"]
 }
