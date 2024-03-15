db00b966bb9a628e41bfb08299106d5dfcbd5660
vravru135 <vravru135@gmail.com>
2024-03-15 09:14:54 +0530

    Adding shell Topics

--- vars.tf
+++ vars.tf
@@ -10,15 +10,4 @@ variable "account_id" {}
 variable "default_vpc_id" {}
 variable "default_route_table_id" {}
 variable "default_vpc_cidr" {}
-variable "rds_allocated_storage" {}
-variable "rds_engine" {}
-variable "rds_engine_version" {}
-variable "rds_instance_class" {}
-variable "backend" {}
-variable "bastion_cidrs" {}
-variable "frontend" {}
-variable "public_alb" {}
-variable "backend_alb" {}
-variable "route53_zone_id" {}
-variable "kms" {}
-variable "certificate_arn" {}
+
