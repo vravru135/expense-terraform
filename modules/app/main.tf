75ab1ed6050a8a77ddfb4642f0bdc7824bb9ee41
vravru135 <vravru135@gmail.com>
2024-03-16 13:13:58 +0530

    Adding shell Topics

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -113,50 +113,6 @@ resource "aws_lb_target_group" "main" {
 }
 
 
-resource "aws_iam_role" "main" {
-  name = "${var.env}-${var.component}"
-  tags = merge(var.tags, { Name = "${var.env}-${var.component}" })
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  inline_policy {
-    name = "my_inline_policy"
-
-    policy = jsonencode({
-      {
-      	"Version": "2012-10-17",
-      	"Statement": [
-      		{
-      			"Sid": "Get Resources",
-      			"Effect": "Allow",
-      			"Action": [
-      				"ssm:GetParameterHistory",
-      				"ssm:GetParametersByPath",
-      				"ssm:GetParameters",
-      				"ssm:GetParameter"
-      			],
-      			"Resource": "arn:aws:ssm:us-east-1:290654222953:parameter/dev.frontend.*"
-      		},
-      		{
-      			"Sid": "List Resources",
-      			"Effect": "Allow",
-      			"Action": "ssm:DescribeParameters",
-      			"Resource": "*"
-      		}
-      	]
-      }
 
 
 
