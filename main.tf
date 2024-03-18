06e9c040359f619f3b09f76c95975d5813bae22d
vravru135 <vravru135@gmail.com>
2024-03-18 11:58:20 +0530

    Adding shell Topics

--- main.tf
+++ main.tf
@@ -42,5 +42,5 @@ module "backend" {
   tags             = var.tags
   vpc_id           = module.vpc.vpc_id
   bastion_cidrs    = var.bastion_cidrs
-  prometheus_cidrs = var.prometheus_cidrs
+  #prometheus_cidrs = var.prometheus_cidrs
 }
