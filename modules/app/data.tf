d5859be47a63646a3eb38718b53d70a2b4128584
vravru135 <vravru135@gmail.com>
2024-03-19 13:14:21 +0530

    Adding shell Topics

--- modules/app/data.tf
+++ modules/app/data.tf
@@ -1,9 +1,3 @@
-data "aws_ami" "main" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["290654222953"]
-}
-
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
